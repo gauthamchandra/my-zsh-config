7f76e6dc921826a40b9ba8c04fc151846c389371
Gautham Chandra <gautham.chandra@greenhouse.io>
2015-12-06 21:02:36 -0500

    Check so vim alias doesn't get applied on Linux

--- config/environment.zsh
+++ config/environment.zsh
@@ -4,7 +4,9 @@ export HISTTIMEFORMAT="%F %T "
 # Use brew libs over system libs
 export PATH=$PATH:/usr/local/bin
 
-# Use brew's version of vim
-alias vim="/usr/local/bin/vim"
+# Use brew's version of vim if we are on OSX
+if [ "$(uname)" = "Darwin" ]; then
+  alias vim="/usr/local/bin/vim"
+fi
 
 export DEFAULT_USER=`whoami`
