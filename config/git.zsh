5d974babe6f3ef3e3176ab0da9bb481141d35658
Gautham Chandra <gautham.chandra@greenhouse.io>
2016-01-12 18:56:07 -0500

    added alias for getting recursive submodules for git

--- config/git.zsh
+++ config/git.zsh
@@ -1,6 +1,9 @@
 # Alias hub as git
 eval "$(hub alias -s)"
 
-alias gpr="git pull --rebase"
+alias gpr='git pull --rebase'
+alias gprr='gpr && git submodule update --init --recursive'
+alias gd='git diff'
 alias gs='git status'
+alias gst='git stash'
 alias gco='git checkout'
