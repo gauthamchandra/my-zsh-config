6cf3e7b1cb9df1a3b2c49c6004d2a8830f956c9b
Gautham Chandra <gautham.chandra@greenhouse.io>
2016-01-22 11:07:30 -0500

    Make sure that osx stuff is inside mac os check

--- config/environment.zsh
+++ config/environment.zsh
@@ -1,12 +1,15 @@
 #Log the command history with the time the command was last executed
 export HISTTIMEFORMAT="%F %T "
 
-# Use brew libs over system libs
-export PATH=$PATH:/usr/local/bin
-
-# Use brew's version of vim if we are on OSX
 if [ "$(uname)" = "Darwin" ]; then
+  # Use brew libs over system libs
+  export PATH=$PATH:/usr/local/bin
+
+  # Use brew's version of vim if we are on OSX
   alias vim="/usr/local/bin/vim"
+
+  # Export brew's version of curl
+  #alias curl="/usr/local/Cellar/curl/7.46.0/bin/curl"
 fi
 
 export DEFAULT_USER=`whoami`
